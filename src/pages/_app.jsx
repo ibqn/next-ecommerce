b111d036a156b288a6d5d8593bd184ebf916f769
Evgeny Bobkin <evgeny.bobkin@europace.de>
2022-12-30 22:48:16 +0100

    working on react query setup

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,5 +1,22 @@
 import 'styles/globals.css'
 
+import {
+  Hydrate,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { useState } from 'react'
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [queryClient] = useState(() => new QueryClient())
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Component {...pageProps} />
+      </Hydrate>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  )
 }
