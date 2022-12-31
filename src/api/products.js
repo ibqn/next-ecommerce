9c59bc8f511089000436d8b587ca63b86dbe8724
ibqn <evgen.ibqn@gmail.com>
2022-12-31 11:58:09 +0100

    filter clothing only

--- src/api/products.js
+++ src/api/products.js
@@ -3,7 +3,7 @@ import { HOST_URI } from './config'
 
 const getProducts = async () => {
   const { data } = await axios.get(`${HOST_URI}/api/products`)
-  return data
+  return data.filter(({ category }) => category.match(/clothing/))
 }
 
 export { getProducts }
