4be87c381953ab1da65ca8ae1a116d34f8fb3619
ibqn <evgen.ibqn@gmail.com>
2023-01-01 16:30:40 +0100

    working on product details

--- src/components/product-details.jsx
+++ src/components/product-details.jsx
@@ -1,7 +1,11 @@
 import { useQuery } from '@tanstack/react-query'
+import { getProduct } from 'api/products'
+import { CartContext } from 'hooks/use-cart'
 import Image from 'next/image'
+import { useContext } from 'react'
 
 export const ProductDetails = ({ id }) => {
+  const { addToCart } = useContext(CartContext)
   const { data, isLoading, error } = useQuery(['product', id], () =>
     getProduct(id)
   )
@@ -10,23 +14,38 @@ export const ProductDetails = ({ id }) => {
     return <div>loading...</div>
   }
 
-  const { title, image } = data
+  const { title, image, price, description } = data
 
   return (
-    <div className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-        <h1>product: {id}</h1>
-        <p>product details: {title}</p>
-        <div>
-          <Image
-            className="h-auto max-w-[100px]"
-            height={300}
-            width={300}
-            src={image}
-            alt={title}
-          />
+    <section className="flex h-screen items-center pt-32 pb-12 lg:py-32">
+      <div className="container mx-auto">
+        <div className="flex flex-col items-center lg:flex-row">
+          <div className="mb-8 flex flex-1 items-center justify-center lg:mb-0">
+            <Image
+              className="h-auto max-w-[200px] lg:max-w-sm"
+              height={600}
+              width={600}
+              src={image}
+              alt={title}
+              priority
+            />
+          </div>
+
+          <div className="flex-1 text-center lg:text-left">
+            <h1 className="mx-auto mb-2 max-w-[450px] text-[26px] font-medium lg:mx-0">
+              {title}
+            </h1>
+            <div className="mb-6 text-xl font-medium">$ {price}</div>
+            <p className="mb-8">{description}</p>
+            <button
+              onClick={() => addToCart(data)}
+              className="bg-primary py-4 px-8 text-white"
+            >
+              Add to cart
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+    </section>
   )
 }
