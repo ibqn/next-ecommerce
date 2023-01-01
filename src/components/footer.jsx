aa6a87820bc6e5be617a808380b77076ad1c410d
ibqn <evgen.ibqn@gmail.com>
2023-01-01 12:14:07 +0100

    complete footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,3 +1,12 @@
 export const Footer = () => {
-  return <div>Footer</div>
+  return (
+    <footer className="bg-primary py-12">
+      <div className="container mx-auto">
+        <p className="text-center text-white">
+          Copyright &copy; Next E-commerce Shop {new Date().getFullYear()}. All
+          rights reserved.
+        </p>
+      </div>
+    </footer>
+  )
 }
